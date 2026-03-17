41c73974efafec238abdfe86593ac3f84986fbca
emse-p4a-gwu <john.helveston@gmail.com>
2026-03-17 18:17:50 -0400

    Update explore.R

--- data-raw/scripts/explore.R
+++ data-raw/scripts/explore.R
@@ -27,3 +27,36 @@ ds %>%
   filter(inventory_type == 'new') %>%
   distinct(make, model, range) %>%
   collect()
+
+dt <- ds %>%
+  filter(make == 'nissan') %>%
+  filter(model %in% c('leaf', 'versa sedan')) %>%
+  filter(year == 2022) %>%
+  filter(inventory_type == 'used') %>%
+  select(price, miles, age_years, model) %>%
+  collect()
+
+dt %>%
+  ggplot(aes(x = miles, y = price, color = model)) +
+  geom_smooth(
+    method = 'lm',
+    se = FALSE
+  ) +
+  geom_point(size = 0.1)
+
+model_leaf <- feols(
+  miles ~ price,
+  data = dt[model == 'leaf']
+)
+model_versa <- feols(
+  miles ~ price,
+  data = dt[model != 'leaf']
+)
+
+dt %>%
+  group_by(model) %>%
+  summarise(
+    price = mean(price),
+    miles = mean(miles),
+    age = mean(age_years)
+  )
